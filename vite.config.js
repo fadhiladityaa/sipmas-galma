bb0fe443ee257bf360212c6e21dfdaf77b5dd64c
fadhiladityaa <fadhiladitya551@gmail.com>
2026-08-27 01:52:09 +0800

    amang

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        hmr: {
-            host: "10.86.115.238", // GANTI DENGAN IP LOKAL KAMU!
-            protocol: "ws",
-        },
-    },
+    // server: {
+    //     host: "0.0.0.0",
+    //     hmr: {
+    //         host: "10.86.115.238", // GANTI DENGAN IP LOKAL KAMU!
+    //         protocol: "ws",
+    //     },
+    // },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
