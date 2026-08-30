b97fb16593bf5b23b323df562637ae00da65d155
fadhiladityaa <fadhiladitya551@gmail.com>
2026-08-30 16:07:04 +0800

    perbaiki route riwayat

--- resources/views/riwayat.blade.php
+++ resources/views/riwayat.blade.php
@@ -94,7 +94,7 @@
                                 d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
                         <p class="text-gama-gray">Belum ada pengajuan surat.</p>
-                        <a href="{{ route('pengajuan-surat') }}"
+                        <a href="{{ route('warga.pengajuan-surat') }}"
                             class="mt-4 inline-block px-4 py-2 bg-gama-primary text-white rounded-lg text-sm hover:bg-[#1f3320] transition">
                             Ajukan Surat Sekarang
                         </a>
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        hmr: {
-            host: "192.168.1.30", // GANTI DENGAN IP LOKAL KAMU!
-            protocol: "ws",
-        },
-    },
+    // server: {
+    //     host: "0.0.0.0",
+    //     hmr: {
+    //         host: "192.168.1.30", // GANTI DENGAN IP LOKAL KAMU!
+    //         protocol: "ws",
+    //     },
+    // },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
