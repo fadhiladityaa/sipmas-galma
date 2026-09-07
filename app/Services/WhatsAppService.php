e32aa1c010703535652891a18f28d4291204c623
fadhiladityaa <fadhiladitya551@gmail.com>
2026-09-07 11:02:06 +0800

    tambahkan baris baru

--- app/Services/WhatsAppService.php
+++ app/Services/WhatsAppService.php
@@ -142,6 +142,7 @@ public function notifyRt($rtPhone, $wargaName, $applicationNumber, $serviceName)
     $message .= "Dari: {$wargaName}\n";
     $message .= "Nomor: {$applicationNumber}\n";
     $message .= "Silakan login ke SIPMAS untuk menyetujui atau menolak pengajuan atau klik link di bawah ini.\n";
+    $message .= "\n";
     $message .= "https://sipmas-galma.my.id";
 
     return $this->sendText($rtPhone, $message);
